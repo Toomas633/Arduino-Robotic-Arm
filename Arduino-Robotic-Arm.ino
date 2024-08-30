c068f64da79091120533a6973125f04fdfe9f074
Toomas <toomas.kirsing@gmail.com>
2024-08-30 13:04:53 +0300

    V2 (#9)
    * Create new variables and change out old ones
    
    * Add Xbox One and 360 controller support
    
    * Solid Edge 3D files
    
    * Optimize
    
    * Remove unneeded files
    
    * Update files for v2 release

--- Arduino-Robotic-Arm.ino
+++ Arduino-Robotic-Arm.ino
@@ -1,14 +1,15 @@
 // Made by Toomas633
-// Still in progress project, code updates can be found in github: https://github.com/Toomas633/Arduino-Robotic-Arm
 
 #include <Servo.h>
 #include <usbhid.h>
 #include <usbhub.h>
 #include <XBOXONE.h>
+#include <XBOXOLD.h>
 
 USB Usb;
 USBHub Hub(&Usb);
 XBOXONE Xb1(&Usb);
+XBOXOLD Xb(&Usb);
 
 Servo servo01;
 Servo servo02;
@@ -24,231 +25,222 @@ int servo4;
 int servo5;
 int servo6;
 
+boolean gripper = false;
+boolean xboxControllerConnected = false;
+boolean psControllerConnected = false;
+
 // Initial positions for servos when powering up and where to home. Change to where you like. Number after "position" means wich servo it corresponds to.
 int initial_position1 = 75;
 int initial_position2 = 175;
 int initial_position3 = 175;
 int initial_position4 = 90;
-int initial_position5 = 0;
-int Grapple_position = 180;
-int GrappleState = 0;
-int ResetButtonState = 0;
+int initial_position5 = 50;
+int initial_position6 = 180;
+
+// Servo sensitivity, how fast they move
+int sen = 5;
 
 void setup()
 {
-  Serial.begin(9600);
-
-  if (Usb.Init() == -1) {
-    Serial.println("USB initialization failed.");
-    while(1);
+  if (Usb.Init() == -1)
+  {
+    while (1)
+      ;
   }
 
-  Serial.println("<Auto homing>");
-
   // Pins to where you connect servos, change here if needed, no need further on.
-  servo01.attach(5);
-  servo02.attach(6);
-  servo03.attach(7);
-  servo04.attach(8);
-  servo05.attach(9);
-  servo06.attach(10);
-
-  delay(20);
+  servo01.attach(2);
+  servo02.attach(3);
+  servo03.attach(4);
+  servo04.attach(5);
+  servo05.attach(6);
+  servo06.attach(7);
 
   servo01.write(initial_position1);
   servo02.write(initial_position2);
   servo03.write(initial_position3);
   servo04.write(initial_position4);
   servo05.write(initial_position5);
-  servo06.write(Grapple_position);
 
-  delay(20);
-
-  Serial.println("<Arm is ready>");
+  // Signal ready status
+  servo06.write(initial_position6);
+  delay(250);
+  servo06.write(100);
+  delay(250);
+  servo06.write(initial_position6);
 }
 
-// All definitions are made here for easy keeping and editing.
-void definitions()
+// All voids here, differnet actoins/inputs have different voids
+void loop()
 {
+  Usb.Task();
+
   servo1 = servo01.read();
   servo2 = servo02.read();
   servo3 = servo03.read();
   servo4 = servo04.read();
   servo5 = servo05.read();
   servo6 = servo06.read();
+
+  int rightX = 100;
+  int rightY = 100;
+  int leftX = 100;
+  int leftY = 100;
+  int upDown = 100;
+
+  boolean xboxConnected = Xb1.XboxOneConnected || Xb.XboxConnected;
+
+  handleControllerConnection(xboxControllerConnected, xboxConnected, "Xbox");
+
+  if (xboxConnected)
+  {
+    if (Xb1.getButtonClick(A) || Xb.getButtonClick(A))
+    {
+      gripper = !gripper;
+      gripper_servo();
+    }
+    if (Xb1.getButtonClick(Y) || Xb.getButtonClick(Y))
+    {
+      auto_home();
+    }
+
+    if (Xb1.XboxOneConnected)
+    {
+      rightX = map(Xb1.getAnalogHat(RightHatX), -32800, 32800, 0, 200);
+      rightY = map(Xb1.getAnalogHat(RightHatY), -32800, 32800, 0, 200);
+      leftX = map(Xb1.getAnalogHat(LeftHatX), -32800, 32800, 0, 200);
+      if (Xb1.getButtonPress(B))
+      {
+        upDown = map(Xb1.getAnalogHat(LeftHatY), -32800, 32800, 0, 200);
+      }
+      else
+      {
+        leftY = map(Xb1.getAnalogHat(LeftHatY), -32800, 32800, 0, 200);
+      }
+    }
+    else
+    {
+      rightX = map(Xb.getAnalogHat(RightHatX), -32800, 32800, 0, 200);
+      rightY = map(Xb.getAnalogHat(RightHatY), -32800, 32800, 0, 200);
+      leftX = map(Xb.getAnalogHat(LeftHatX), -32800, 32800, 0, 200);
+      if (Xb.getButtonPress(B))
+      {
+        upDown = map(Xb.getAnalogHat(LeftHatY), -32800, 32800, 0, 200);
+      }
+      else
+      {
+        leftY = map(Xb.getAnalogHat(LeftHatY), -32800, 32800, 0, 200);
+      }
+    }
+  }
+
+  right_joystick(rightX, rightY);
+  left_joystick(leftX, leftY);
+  wrist_servo(upDown);
+
+  delay(20);
 }
 
-// All voids here, differnet actoins/inputs have different voids
-void loop()
+//=========================== Controller connection logic =================================
+void handleControllerConnection(boolean &isConnected, boolean isCurrentlyConnected, const char *controllerName)
 {
-  // reset();
-  // definitions();
-  // left_joystick();
-  // right_joystick();
-  // wrist_servo();
-  // grapple_servo();
+  if (isCurrentlyConnected != isConnected)
+  {
+    isConnected = isCurrentlyConnected;
+  }
 }
 
 //============================== Right Joystick ===========================================
-void right_joystick()
+void right_joystick(int x, int y)
 {
-  if (RightJoystickXVal < 300)
+  if (x < 70)
   {
-    if (initial_position4 < 10)
-    {
-    }
-    else
-    {
-      initial_position4 = constrain(initial_position4 - 5, 5, 175);
-      servo04.write(initial_position4);
-    }
+    initial_position4 = constrain(initial_position4 - map(x, 70, 0, 0, sen), 0, 180);
+    servo04.write(initial_position4);
   }
-  if (RightJoystickXVal > 700)
+  if (x > 130)
   {
-    if (initial_position4 > 180)
-    {
-    }
-    else
-    {
-      initial_position4 = constrain(initial_position4 + 5, 5, 175);
-      servo04.write(initial_position4);
-    }
+    initial_position4 = constrain(initial_position4 + map(x, 130, 200, 0, sen), 0, 180);
+    servo04.write(initial_position4);
   }
-  if (RightJoystickYVal < 300)
+  if (y < 70)
   {
-    if (initial_position3 < 10)
-    {
-    }
-    else
-    {
-      initial_position3 = constrain(initial_position3 + 8, 5, 175);
-      servo03.write(initial_position3);
-      delay(20);
-    }
+    initial_position3 = constrain(initial_position3 - map(x, 70, 0, 0, sen), 5, 175);
+    servo03.write(initial_position3);
   }
-  if (RightJoystickYVal > 700)
+  if (y > 130)
   {
-    if (initial_position3 > 180)
-    {
-    }
-    else
-    {
-      initial_position3 = constrain(initial_position3 - 8, 5, 175);
-      servo03.write(initial_position3);
-      delay(20);
-    }
+    initial_position3 = constrain(initial_position3 + map(x, 130, 200, 0, sen), 5, 175);
+    servo03.write(initial_position3);
   }
 }
 
 //================================== Left Joystick =============================================
-void left_joystick()
+void left_joystick(int x, int y)
 {
-  if (LeftJoystickXVal < 300)
+  if (x < 70)
   {
-    if (initial_position1 < 10)
-    {
-    }
-    else
-    {
-      initial_position1 = constrain(initial_position1 + 8, 10, 160);
-      servo01.write(initial_position1);
-      delay(20);
-    }
-  }
-  if (LeftJoystickXVal > 700)
-  {
-    if (initial_position1 > 180)
-    {
-    }
-    else
-    {
-      initial_position1 = constrain(initial_position1 - 8, 10, 160);
-      servo01.write(initial_position1);
-      delay(20);
-    }
-  }
-  if (LeftJoystickYVal < 300)
-  {
-    if (initial_position2 < 10)
-    {
-    }
-    else
-    {
-      initial_position2 = constrain(initial_position2 - 8, 5, 175);
-      servo02.write(initial_position2);
-      delay(20);
-    }
-  }
-  if (LeftJoystickYVal > 700)
-  {
-    if (initial_position2 > 180)
-    {
-    }
-    else
-    {
-      initial_position2 = constrain(initial_position2 + 8, 5, 175);
-      servo02.write(initial_position2);
-      delay(20);
-    }
-  }
-}
-
-//================================= Reset button ========================================
-void reset()
-{
-  if (ResetButtonState = HIGH)
-  {
-    Serial.println("<Auto homing>");
-    delay(20);
-    initial_position1 = 75;
-    initial_position2 = 175;
-    initial_position3 = 175;
-    initial_position4 = 90;
-    Grapple_position = 180;
-    delay(20);
+    initial_position1 = constrain(initial_position1 + map(x, 70, 0, 0, sen), 5, 175);
     servo01.write(initial_position1);
+  }
+  if (x > 120)
+  {
+    initial_position1 = constrain(initial_position1 - map(x, 130, 200, 0, sen), 5, 175);
+    servo01.write(initial_position1);
+  }
+  if (y < 70)
+  {
+    initial_position2 = constrain(initial_position2 - map(x, 70, 0, 0, sen), 5, 175);
     servo02.write(initial_position2);
-    servo03.write(initial_position3);
-    servo04.write(initial_position4);
-    servo06.write(Grapple_position);
+  }
+  if (y > 130)
+  {
+    initial_position2 = constrain(initial_position2 + map(x, 130, 200, 0, sen), 5, 175);
+    servo02.write(initial_position2);
   }
 }
 
 //================================= Wrist servo =========================================
-void wrist_servo()
+void wrist_servo(int value)
 {
-  servo05.write(180 - (Wrist + 520) / 10);
-  delay(20);
-}
-//============================== Grapple servo ===========================================
-void grapple_servo()
-{
-  if (GrappleState = HIGH)
+  if (value > 130)
   {
-    servo06.write(0);
-    delay(20);
+    initial_position5 = constrain(initial_position5 + map(value, 130, 200, 0, sen), 5, 175);
+    servo05.write(initial_position5);
+  }
+  if (value < 70)
+  {
+    initial_position5 = constrain(initial_position5 - map(value, 70, 0, 0, sen), 5, 175);
+    servo05.write(initial_position5);
+  }
+}
+
+//================================= Auto home ========================================
+void auto_home()
+{
+  initial_position1 = 75;
+  initial_position2 = 175;
+  initial_position3 = 175;
+  initial_position4 = 90;
+  initial_position5 = 50;
+  initial_position6 = 180;
+  servo01.write(initial_position1);
+  servo02.write(initial_position2);
+  servo03.write(initial_position3);
+  servo04.write(initial_position4);
+  servo05.write(initial_position5);
+  servo06.write(initial_position6);
+}
+
+//============================== Grapple servo ===========================================
+void gripper_servo()
+{
+  if (gripper)
+  {
+    servo06.write(100);
   }
   else
   {
     servo06.write(180);
-    delay(20);
-  }
-}
-
-//============================= Read controller ========================================
-void read_controller() {
-  Usb.Task();
-  if (Xb1.XboxOneConnected) {
-    int leftX = Xb1.getAnalogHat(LeftHatX);
-    int leftY = Xb1.getAnalogHat(LeftHatY);
-    int rightX = Xb1.getAnalogHat(RightHatX);
-    int rightY = Xb1.getAnalogHat(RightHatY);
-
-    if (Xb1.getButtonClick(A)) {
-      Serial.println("A button pressed.");
-    }
-    if (Xb1.getButtonPress(B)) {
-      Serial.println("B button pressed.");
-    }
   }
 }
